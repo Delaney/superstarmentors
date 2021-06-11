8a46b532db94aa0be9f9831579563e8aba0ced0a
Delaney Sylvans <delaneysylvans@gmail.com>
2021-06-11 23:36:42 +0100

    create transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'broadcast_id',
+        'paystack_reference',
+        'transaction_type',
+        'payment_method',
+        'payment_status',
+        'notes',
+        'amount',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
---
+++ database/migrations/2021_06_10_203110_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('broadcast_id')->nullable();
+            $table->string('paystack_reference')->nullable();
+            $table->enum('transaction_type', ['credit', 'debit']);
+            $table->enum('payment_method', ['wallet', 'card', 'bank', 'ussd']);
+            $table->string('payment_status')->default('pending');
+            $table->text('notes')->nullable();
+            $table->decimal('amount', 10, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
