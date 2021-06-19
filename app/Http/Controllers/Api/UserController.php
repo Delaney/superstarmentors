436575cf65a80c46169fc3591b7c1958271e9cf0
Delaney Sylvans <delaneysylvans@gmail.com>
2021-06-19 13:29:57 +0100

    user login

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -14,8 +14,7 @@ use App\Models\{
 
 class UserController extends Controller
 {
-    public function signup(Request $request)
-    {
+    public function signup(Request $request) {
         $validator = Validator::make(
             $request->all(),
             [
@@ -51,6 +50,41 @@ class UserController extends Controller
                 'email'         => $user->email,
                 'api_token'     => $user->api_token
             ]
+        ]);
+    }
+
+    public function login(Request $request) {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email'     => 'email|required',
+                'password'  => 'required',
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error'     => 'invalid_input',
+                'message'   => $validator->errors()->first()
+            ], 400);
+        }
+
+        $user = User::where('email', $request->input('email'))->first();
+
+        if (!$user || !Hash::check($request->input('password'), $user->password) || $user->account_type != 'user') {
+            return response()->json([
+                'error'     => 'invalid_login',
+                'message'   => 'The email/password provided is incorrect'
+            ], 401);
+        }
+
+        $user->login();
+
+        return response()->json([
+            'id'            => $user->id,
+            'name'          => $user->getFullName(),
+            'email'         => $user->email,
+            'api_token'     => $user->api_token,
         ]);
     }
 }
